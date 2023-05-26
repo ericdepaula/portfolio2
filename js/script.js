f02ad07ccc8c7a5850ca66f893942e43b881a2ed
ericdepaula <ericdepaula39@gmail.com>
2023-05-26 14:37:39 -0300

    JavaScript

---
+++ js/script.js
@@ -0,0 +1,65 @@
+// ICONE NAV BAR
+
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+// Verificando se a parte onde esta o site SCROLL esta de acordo com o nav, deixando ativo a parte onde passar o SCROLL
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*= " + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+
+  //   stick navbar
+
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+// Scroll reveal
+
+ScrollReveal({ 
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portifolio-box, .contato form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img ', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content ', { origin: 'right' });
+
+// Typed JS
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Desenvolvedor FrontEnd', 'Professor de Skate'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
